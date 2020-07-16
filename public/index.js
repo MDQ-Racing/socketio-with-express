7f6bc9901ffe99b21d56d80b808bc8fb922b8fc6
Luciano Bustos <luchyx@gmail.com>
2020-07-16 10:56:44 +0200

    adding a serverside chart data provider

--- public/index.js
+++ public/index.js
@@ -1,36 +1,78 @@
-
 async function ajax(url) {
-    return new Promise((resolve, reject) => {
-        const request = new XMLHttpRequest();
-        request.addEventListener("load", function () {
-            try {
-                resolve(this.responseText);
-            } catch (error) {
-                reject(error);
-            }
-        });
-        request.open("GET", url);
-        request.send();
-        request.addEventListener("error", reject)
+  return new Promise((resolve, reject) => {
+    const request = new XMLHttpRequest();
+    request.addEventListener("load", function () {
+      try {
+        resolve(this.responseText);
+      } catch (error) {
+        reject(error);
+      }
     });
+    request.open("GET", url);
+    request.send();
+    request.addEventListener("error", reject);
+  });
 }
-
+let socket;
 /** @returns {void} */
 async function main() {
-    // call sample API
-    document.getElementById("random-number").innerText = await ajax("/random");
+  // call sample API
+  document.getElementById("random-number").innerText = await ajax("/random");
+}
+main();
 
-    const socket = io();
-    socket.on("connect", () => socket.emit("hello", `Hi there! I am ${window.navigator.userAgent}`));
-
-    const secondsElement = document.getElementById("seconds");
-    socket.on("seconds", seconds => secondsElement.innerText = seconds.toString());
-
-    const welcomeElement = document.getElementById("welcome");
-    socket.on("online", online => onlineElement.innerText = online.toString());
-
-    const onlineElement = document.getElementById("online");
-    socket.on("welcome", welcomeMessage => welcomeElement.innerText = welcomeMessage);
+function renderData(sd) {
+  data.rows.push([data.rows.length, sd]);
+  chart.data(data);
+  //chart.draw();
 }
 
-main();
+function disconnect() {
+  socket.disconnect();
+}
+function connect() {
+  socket = io("/my-namespace");
+  socket.on("connect", () =>
+    socket.emit("hello", `Hi there! I am ${window.navigator.userAgent}`)
+  );
+
+  const secondsElement = document.getElementById("seconds");
+  socket.on(
+    "seconds",
+    (seconds) => (secondsElement.innerText = seconds.toString())
+  );
+
+  const welcomeElement = document.getElementById("welcome");
+  socket.on(
+    "online",
+    (online) => (onlineElement.innerText = online.toString())
+  );
+  socket.on("greetings", (msj) => alert(msj));
+  socket.on("onDataRead", (data) => renderData(data));
+
+  const onlineElement = document.getElementById("online");
+  socket.on(
+    "welcome",
+    (welcomeMessage) => (welcomeElement.innerText = welcomeMessage)
+  );
+
+  socket.on("disconnect", () => {
+    onlineElement.innerText = "";
+    data.rows = [];
+    chart.data(data);
+  });
+}
+var chart;
+var data;
+anychart.onDocumentReady(function () {
+  // the data
+  data = {
+    rows: [],
+  };
+  // create the chart
+  chart = anychart.column();
+  // add the data
+  chart.title("The deadliest earthquakes in the XXth century");
+  chart.container("container");
+  chart.draw();
+});
